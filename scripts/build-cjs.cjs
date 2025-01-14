64332838674d90f67d75f0240f0ab342d7eba2e8
AlexAGD <aliaksei.dalhapolau@gmail.com>
2025-01-15 01:01:27 +0300

    fix test

--- scripts/build-cjs.cjs
+++ scripts/build-cjs.cjs
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const esmCode = fs.readFileSync(path.join(__dirname, '../dist/index.js'), 'utf8');
 
 const cjsCode = `
@@ -5,6 +8,14 @@ const cjsCode = `
 Object.defineProperty(exports, "__esModule", { value: true });
 
 ${esmCode.replace('export default', 'module.exports = ')}
+const path = require('path');
+
+${esmCode
+  .replace(/import\s+{\s*([^}]+)\s*}\s+from\s+['"]([^'"]+)['"]/g, 'const { $1 } = require("$2")')
+  .replace('export default', 'module.exports =')
+  .replace(/export\s+function\s+(\w+)/g, 'exports.$1 = function $1')
+  .replace(/export\s+class\s+(\w+)/g, 'exports.$1 = class $1')
+}
 `;
 
 fs.writeFileSync(path.join(__dirname, '../dist/index.cjs'), cjsCode);
