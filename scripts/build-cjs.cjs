5e55a7dc41e8dee1bac1a210cac744b499ead892
AlexAGD <aliaksei.dalhapolau@gmail.com>
2025-01-15 01:03:13 +0300

    fix test

--- scripts/build-cjs.cjs
+++ scripts/build-cjs.cjs
@@ -1,14 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-const esmCode = fs.readFileSync(path.join(__dirname, '../dist/index.js'), 'utf8');
+const distPath = path.join(__dirname, '../dist');
+
+// Убедимся, что директория dist существует
+if (!fs.existsSync(distPath)) {
+  fs.mkdirSync(distPath, { recursive: true });
+}
+
+const esmCode = fs.readFileSync(path.join(distPath, 'index.js'), 'utf8');
 
 const cjsCode = `
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-
-${esmCode.replace('export default', 'module.exports = ')}
-const path = require('path');
 
 ${esmCode
   .replace(/import\s+{\s*([^}]+)\s*}\s+from\s+['"]([^'"]+)['"]/g, 'const { $1 } = require("$2")')
@@ -18,4 +22,4 @@ ${esmCode
 }
 `;
 
-fs.writeFileSync(path.join(__dirname, '../dist/index.cjs'), cjsCode);
+fs.writeFileSync(path.join(distPath, 'index.cjs'), cjsCode);
