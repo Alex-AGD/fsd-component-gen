0a7304571ca40bd96cb95b3fcee6ebefa5a62c59
AlexAGD <aliaksei.dalhapolau@gmail.com>
2025-01-15 00:00:06 +0300

    fix eslint

---
+++ jest.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^(\\.{1,2}/.*)\\.ts$': '$1'
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: './tsconfig.test.json'
+      }
+    ]
+  },
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/']
+}; 
