b364d42b881ea0113829881a19c2f197cc673186
AlexAGD <aliaksei.dalhapolau@gmail.com>
2025-01-15 01:05:41 +0300

    Revert "fix test"
    This reverts commit 5e55a7dc41e8dee1bac1a210cac744b499ead892.

--- scripts/build-cjs.cjs
+++ scripts/build-cjs.cjs
@@ -1,18 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-const distPath = path.join(__dirname, '../dist');
-
-// Убедимся, что директория dist существует
-if (!fs.existsSync(distPath)) {
-  fs.mkdirSync(distPath, { recursive: true });
-}
-
-const esmCode = fs.readFileSync(path.join(distPath, 'index.js'), 'utf8');
+const esmCode = fs.readFileSync(path.join(__dirname, '../dist/index.js'), 'utf8');
 
 const cjsCode = `
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+
+${esmCode.replace('export default', 'module.exports = ')}
+const path = require('path');
 
 ${esmCode
   .replace(/import\s+{\s*([^}]+)\s*}\s+from\s+['"]([^'"]+)['"]/g, 'const { $1 } = require("$2")')
@@ -22,4 +18,4 @@ ${esmCode
 }
 `;
 
-fs.writeFileSync(path.join(distPath, 'index.cjs'), cjsCode);
+fs.writeFileSync(path.join(__dirname, '../dist/index.cjs'), cjsCode);
